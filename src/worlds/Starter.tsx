c2ec42dcdf3ec351a15de25efccd3ef7f5fc953c
adamwatters <adamwatters11@gmail.com>
2021-11-26 19:28:10 -0500

    added phone video

--- src/worlds/Starter.tsx
+++ src/worlds/Starter.tsx
@@ -1,10 +1,12 @@
 import { Spinning, Floating, StandardEnvironment, Audio, Arrow } from "spacesvr";
+import { Suspense } from "react";
+import Phone from "../phone.js"
 import TransparentFloor from "../ideas/TransparentFloor";
 import CloudySky from "../ideas/CloudySky";
 
 export default function Starter() {
   return (
-    <StandardEnvironment playerProps={{ pos: [5, 5, 5], controls: {disableGyro: true}}}>
+    <StandardEnvironment playerProps={{ pos: [0, 20, 20], controls: {disableGyro: true}}}>
       <ambientLight />
       <group position={[10, 1, 0]}>
         <Arrow dark={true} />
@@ -12,22 +14,21 @@ export default function Starter() {
       </group>
       <group position={[10, 1, 0]}>
         <Audio
-          url="https://cdn.pixabay.com/download/audio/2021/10/29/audio_dee1428e39.mp3?filename=tense-detective-looped-drone-10054.mp3"
+          url="https://cdn.pixabay.com/download/audio/2021/11/23/audio_b4dc76f582.mp3?filename=time-to-relax-11152.mp3"
           position={[0, 0, 0]}
           volume={1}
           rollOff={1}
         />
         <Arrow dark={true} />
       </group>
-      <group position={[0, 2, 0]}>
-        <Floating>
-          <Spinning xSpeed={0.2} ySpeed={0.4} zSpeed={0.3}>
-            <mesh>
-              <torusKnotBufferGeometry args={[1, 0.2]} />
-              <meshStandardMaterial color="blue" />
-            </mesh>
-          </Spinning>
-        </Floating>
+      <group position={[0, 3, 0]}>
+          <Suspense fallback={null}>
+            <Phone
+              offsetAngle={0}
+              offsetVertical={0}
+              src="/create.mp4"
+            />
+          </Suspense>
       </group>
       <CloudySky color="white" />
       <TransparentFloor opacity={0.7} />
